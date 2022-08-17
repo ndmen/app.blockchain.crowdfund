e4adc3e9e25d088fcb95081159d76fae614b0876
Artem Abdullin <artemleelooai@gmail.com>
2022-08-17 23:56:12 +0300

    Added events

--- CroudFund.sol
+++ CroudFund.sol
@@ -11,3 +11,15 @@ interface IERC20 {
     ) external returns (bool);
 }
 
+contract CrowdFund {
+    event Launch(
+        uint id,
+        address indexed creator,
+        uint goal,
+        uint32 startAt,
+        uint32 endAt
+    );
+    event Cancel(uint id);
+    event Pledge(uint indexed id, address indexed caller, uint amount);
+}
+
